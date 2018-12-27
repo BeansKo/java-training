5799c15f71d822c1b3dba3e070acc5cacfff515d
fl76 <fl76@newegg.com>
2018-12-27 15:36:01 +0800

    add

---
+++ src/main/java/com/beans/ko/java/training/App.java
@@ -0,0 +1,12 @@
+package com.beans.ko.java.training;
+
+import java.util.Date;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println(new Date().getTime());
+		System.out.println(System.currentTimeMillis());
+	}
+
+}
