e2a33c3ca4905479cb2e05db4a9f1b358da5d7c0
fl76 <fl76@newegg.com>
2018-12-29 16:27:59 +0800

    no message

--- src/main/java/com/beans/ko/java/training/App.java
+++ src/main/java/com/beans/ko/java/training/App.java
@@ -1,12 +1,15 @@
 package com.beans.ko.java.training;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class App {
 
 	public static void main(String[] args) {
+		
 		System.out.println(new Date().getTime());
 		System.out.println(System.currentTimeMillis());
+		System.out.print(LocalDate.now());
 	}
 
 }
