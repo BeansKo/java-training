3bbe9918220a27f61697ce0e501a58e36acc4d5c
fl76 <fl76@newegg.com>
2019-01-05 15:50:36 +0800

    no message

--- src/main/java/com/beans/ko/java/training/entity/ShouJi.java
+++ src/main/java/com/beans/ko/java/training/entity/ShouJi.java
@@ -1,6 +1,6 @@
 package com.beans.ko.java.training.entity;
 
-public class ShouJi{
+public class ShouJi implements Comparable<ShouJi>{
 	private String id;
 	private String name;
 	
@@ -37,6 +37,40 @@ public class ShouJi{
 	public String toString() {
 		return "手机：[ID,"+id+";NAME:"+name+"]";
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ShouJi other = (ShouJi) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(ShouJi o) {
+		return this.id.hashCode()- o.hashCode();
+	}
 }
