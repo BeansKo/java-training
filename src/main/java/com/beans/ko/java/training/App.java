d0de749811fc548c4240a8cecbf06d920ac9f4c0
fl76 <fl76@newegg.com>
2019-02-14 10:23:40 +0800

    no message

--- src/main/java/com/beans/ko/java/training/App.java
+++ src/main/java/com/beans/ko/java/training/App.java
@@ -2,14 +2,17 @@ package com.beans.ko.java.training;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.UUID;
 
 public class App {
-
+	private Class<?> clazz = getClass();
 	public static void main(String[] args) {
-		
+		App app = new App();
 		System.out.println(new Date().getTime());
 		System.out.println(System.currentTimeMillis());
 		System.out.print(LocalDate.now());
+		System.out.println(app.clazz);
+		System.out.println(UUID.randomUUID().toString().replaceAll("-", ""));
+		System.out.println(Thread.currentThread().getContextClassLoader().getResource(""));
 	}
-
 }
